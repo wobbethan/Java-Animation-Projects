bf6e2a9258b48d3067c0b6b318825a6bdc2099a7
wobbethan <wobbethan@gmail.com>
2024-03-13 18:22:51 -0400

    Micro animations done

---
+++ Micro Animations/app.js
@@ -0,0 +1,57 @@
+const tl = gsap.timeline({
+  default: { duration: 0.35, ease: "Power2.easeOut" },
+});
+const home = document.querySelector(".home");
+const notifications = document.querySelector(".notifications");
+const messages = document.querySelector(".messages");
+
+gsap.set(".feather", { scale: 0, transformOrigin: "center" });
+//home animation
+home.addEventListener("click", () => {
+  gsap.fromTo(".home-svg", { scale: 1 }, { scale: 0.9, yoyo: true, repeat: 1 });
+  gsap.fromTo(
+    ".feather",
+    { scale: 0, y: -5 },
+    { scale: 1.5, y: 20, duration: 1, stagger: 0.2 }
+  );
+  gsap.fromTo(".right-feather", { x: 0 }, { x: 5 });
+  gsap.fromTo(".left-feather", { x: 0 }, { x: -5 });
+});
+
+//Notification animation
+gsap.set(".bell", { transformOrigin: "top center" });
+gsap.set(".ringer", { transformOrigin: "top center" });
+gsap.set(".wave", { opacity: 0, transformOrigin: "bottom" });
+
+notifications.addEventListener("click", () => {
+  gsap.fromTo(
+    ".bell",
+    { rotation: -5 },
+    { rotation: 0, duration: 2, ease: "elastic.out(2,0.2)" }
+  );
+  gsap.fromTo(
+    ".ringer",
+    { rotation: -3, x: 0.7 },
+    { rotation: 0, x: 0, duration: 2, ease: "elastic.out(2,0.2)" }
+  );
+  gsap.fromTo(
+    ".wave",
+    { scale: 0, opacity: 1 },
+    { scale: 1.3, opacity: 0, duration: 1 }
+  );
+});
+
+//messages
+gsap.set(".flap", { transformOrigin: `top` });
+
+messages.addEventListener("click", () => {
+  tl.fromTo(".messages-svg", { scale: 1 }, { scale: 0.9 });
+  tl.fromTo(".flap", { scale: 1 }, { scale: -1 }, "<50%");
+  tl.fromTo(".messages-svg", { scale: 0.9 }, { scale: 1 }, "<50%");
+  tl.fromTo(
+    ".note",
+    { y: 0, opacity: 1 },
+    { y: -40, opacity: 0, duration: 0.75 }
+  );
+  tl.to(".flap", { scale: 1 });
+});
